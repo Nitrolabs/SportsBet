de7d00f18ef29b76f4618224da558c7627e0f98a
Max Ferguson <maxkferg@gmail.com>
2015-01-18 15:36:30 -0800

    Tidied up the side menu

--- client/app.js
+++ client/app.js
@@ -40,3 +40,31 @@ App.helpers = {
 _.each(App.helpers, function (helper, key) {
   Handlebars.registerHelper(key, helper);
 });
+
+
+// HACK: Disable the left side menu and add PC support
+// TODO: Decide whether we like the side menu
+// TODO: Rewrite this code, and move it somewhere else
+Template.mobileLayout.rendered = function(){
+    IonSideMenu.snapper.settings({
+        disable:'left',
+        dragger: document.getElementById('side-menu-button'),
+        hyperextensible: false
+    });
+    $(document).on('click', '#side-menu-button:not(.active)',function(){
+        IonSideMenu.snapper.expand();
+        $('#side-menu-button').addClass('active');
+        $('.snap-drawer-right').width('256px');
+    });
+
+    $(document).on('click', '#side-menu-button.active',function(){
+        IonSideMenu.snapper.close();
+        $('#side-menu-button').removeClass('active');
+    });
+}
+
+
+
+
+
+
