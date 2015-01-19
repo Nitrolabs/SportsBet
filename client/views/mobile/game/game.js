3e04f41d001493afdfb1f20e6005aa520e03384d
Max Ferguson <maxkferg@gmail.com>
2015-01-18 18:55:20 -0800

    Added boilerplate for collection hooks

--- client/views/mobile/game/game.js
+++ client/views/mobile/game/game.js
@@ -94,6 +94,26 @@ Template.MobileGame.helpers({
     }
 });
 
+/*****************************************************************************/
+/* Game: Collection Hooks */
+/*****************************************************************************/
+
+/*
+ * https://github.com/matb33/meteor-collection-hooks
+ * Meteor.users.after.update(function(userId, doc, fieldNames, modifier, options){
+ *       if (money went up){
+ *           MAX: Write UI code
+ *       } else
+ *           MAX: Write UI code
+ *       });
+ *
+ *       if (status_message_changed){
+ *            MAX: Write UI code
+ *       }
+ * })
+
+
+
 
 /*****************************************************************************/
 /* Game: Lifecycle Hooks */
