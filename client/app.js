3128017db752e44d098ccf93b16b72d584dbfcb4
Assaf Einat <assaf.einat@gmail.com>
2015-01-31 21:12:17 +0000

    Merge with master

--- client/app.js
+++ client/app.js
@@ -122,23 +122,4 @@ initMixpanel = function() {
 initMixpanel();
 
 
-// HACK: Disable the left side menu and add PC support
-// TODO: Decide whether we like the side menu
-// TODO: Rewrite this code, and move it somewhere else
-Template.mobileLayout.rendered = function(){
-    IonSideMenu.snapper.settings({
-        disable:'left',
-        dragger: document.getElementById('side-menu-button'),
-        hyperextensible: false
-    });
-    $(document).on('click', '#side-menu-button:not(.active)',function(){
-        IonSideMenu.snapper.expand();
-        $('#side-menu-button').addClass('active');
-        $('.snap-drawer-right').width('256px');
-    });
 
-    $(document).on('click', '#side-menu-button.active',function(){
-        IonSideMenu.snapper.close();
-        $('#side-menu-button').removeClass('active');
-    });
-}
--- client/views/mobile/leaderboard/leaderboard.js
+++ client/views/mobile/leaderboard/leaderboard.js
@@ -5,43 +5,10 @@ Template.Leaderboard.events({
 });
 
 Template.Leaderboard.helpers({
-<<<<<<< HEAD
-    getUserLeaderboard: function() {
-        var maxUsersInLeaderBoard = Session.get('maxUsersInLeaderBoard') || 3;
-        var t = Meteor.users.find({},{sort: {bank_account: -1}, limit: maxUsersInLeaderBoard})
-        
-        var amIinTop10 = false;
-        t.forEach(function(y) {amIinTop10 |= (y._id == Meteor.userId());});
-        Session.set('LeaderboardAmIinTop10', amIinTop10);
-        
-        return t;
-    },
-    highlightMyselfOnTable: function() {
-        return (this._id == Meteor.userId());
-    },
-    getPositionsInLeaderboard: function(id) {
-        var self = this;
-        var index = 0;
-        var t = Meteor.users.find({},{sort: {bank_account: -1}});
-        var found = false;
-        t.forEach(function(y) {
-            if (!found) {
-                index++;
-                found = (y._id == id);
-            }
-        });
-        
-        // if (self._id == Meteor.userId()) {
-        //     App.track("My place in the leadeboard", {place:index});
-        // }
-        
-        return index;
-    }
-=======
+
   leaderboard: function(){
     // ASSAF: Return a list that we can generate the leaderboard from
   }
->>>>>>> d5c5f747f2e331aa21ca9f9608c968a9882d95f6
 });
 
 
