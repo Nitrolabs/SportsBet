cd269af96598a6c083fd18867d8c6e622ba8a16d
Assaf Einat <assaf.einat@gmail.com>
2015-02-01 00:45:51 +0000

    History page

--- client/views/mobile/history/history.js
+++ client/views/mobile/history/history.js
@@ -29,15 +29,16 @@ Template.History.helpers({
     var user_history_bets = UserBets.find({user_id: Meteor.userId(), bet_id: {$in: list_history}}, {sort: {resolved_at: -1}}).fetch();
     user_history_bets.forEach(function(x) {
         var b = Bets.findOne(x.bet_id);
-        console.log("x");
-        console.log(x);
-        console.log("b");
-        console.log(b);
         
         var correct_answer = (b.actual_result > 0) ? b.outcomes[b.actual_result - 1].text : "<Other>";
         var user_answer = (x.skipped == true) ? "<Skipped>" : b.outcomes[x.answer - 1].text;
+        var profit = -x.wager;
+        if (!x.skipped && x.answer == b.actual_result && x.answer > 0) {
+            profit += b.outcomes[x.answer - 1].odds * x.wager;
+        } 
         _.extend(x, {question: b.question, status_update: b.status_update, actual_result: b.actual_result, 
-                     is_correct: (x.answer == b.actual_result), user_answer: user_answer, correct_answer: correct_answer});
+                     is_correct: (x.answer == b.actual_result), user_answer: user_answer, correct_answer: correct_answer,
+                     profit: profit});
     });
     return user_history_bets;
   }
